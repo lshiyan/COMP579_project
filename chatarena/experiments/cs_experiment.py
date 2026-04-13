40acb42a7c0e129a7d7e08d5c2bb52b31b911026
Shiyan <shiyan.wliu@gmail.com>
2026-04-12 22:12:52 -0400

    Adding in experiment files.

---
+++ chatarena/experiments/cs_experiment.py
@@ -0,0 +1,30 @@
+from chatarena.chatarena.arena import Arena
+from chatarena.chatarena.config import ArenaConfig, BackendConfig
+
+BACKEND_CONFIGS = {
+    "openai-chat": {
+        "backend_type": "openai-chat",
+        "temperature": 0.9,
+        "max_tokens": 100
+    },
+    "claude": {
+        "backend_type": "claude",
+        "temperature": 0.9,
+        "max_tokens": 100
+    },
+    "gemini": {
+        "backend_type": "gemini",
+        "temperature": 0.9,
+        "max_output_tokens": 100
+    }
+}
+
+class ClosedSourceExperiment():
+    
+    def __init__(self, experiment_filepath: str, backend_name: str):
+        
+        self.experiment_filepath = experiment_filepath
+        self.arena_config = ArenaConfig.load(experiment_filepath)
+
+        for player in self.arena_config.players:
+            player.backend = BackendConfig(BACKEND_CONFIGS[backend_name])
